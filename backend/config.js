42f954a6f9b44dc9ef6df4696a8cf7a5ac0c58d3
Swetha <61079681+SwethaSarah@users.noreply.github.com>
2021-02-12 01:00:58 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export default {
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://Swetha:Admin123@onlineshoping.7r2zf.mongodb.net/Shopping?retryWrites=true&w=majority',
+    JWT_SECRET: process.env.JWT_SECRET || '3f1e370905701af3cae77bb67da8efb1c1db2965ebc3d2a17047fe6a922f262b27e5070afe9493eeb6051992aeca5e60401335ddc002adf5f2b7275aed81e352'
+}
