63e046ba1c4a1700bb298c0a740a0585556d15c4
Swetha <61079681+SwethaSarah@users.noreply.github.com>
2021-02-12 16:14:42 +0530

    Update AddProductScreen.js

--- frontend/src/Screens/AddProductScreen.js
+++ frontend/src/Screens/AddProductScreen.js
@@ -106,23 +106,23 @@ function AddProductScreen(props){
           <form  encType="multipart/form-data" onSubmit={submitHandler}>
             {loadingSave?<div>Loading</div>:<div></div>}
             {errorSave?<div>{error}</div>:<div></div>}
-            <label>Product Id:</label>
-            <input type="text" name="id" id="id" value={id} onChange={(e) => setId(e.target.value)}></input>
-            <label>Name of the product:</label>
-            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)}></input>
-            <label>Image of the product</label>
-            <input type="file" id="image" name="image" onChange={imagehandler} required/> 
-            <label>Price of the product</label>
-            <input type="number" name="price" id="price" value={price} onChange={(e) => setPrice(e.target.value)}></input>
-            <label>Brand of the product:</label>
-            <input type="text" name="brand" id="brand" value={brand} onChange={(e) => setBrand(e.target.value)}></input>
-            <label>Category of the product:</label>
-            <input type="text" name="category" id="category" value={category} onChange={(e) => setCategory(e.target.value)}></input>
-            <label>Product description:</label>
-            <textarea name="description" id="description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
-            <label>Number of product in stock:</label>
-            <input type="number" name="countAvailable" id="countAvailable" value={countAvailable} onChange={(e) => setCountAvailable(e.target.value)}></input>
-          <button className="primaryBtn" name="save">{id ?"Update": "Add"}</button>
+            <label>Product Id:</label><br/>
+            <input type="text" name="id" id="id" value={id} onChange={(e) => setId(e.target.value)}></input><br/>
+            <label>Name of the product:</label><br/>
+            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)}></input><br/>
+            <label>Image of the product</label><br/>
+            <input type="file" id="image" name="image" onChange={imagehandler} required/><br/>
+            <label>Price of the product</label><br/>
+            <input type="number" name="price" id="price" value={price} onChange={(e) => setPrice(e.target.value)}></input><br/>
+            <label>Brand of the product:</label><br/>
+            <input type="text" name="brand" id="brand" value={brand} onChange={(e) => setBrand(e.target.value)}></input><br/>
+            <label>Category of the product:</label><br/>
+            <input type="text" name="category" id="category" value={category} onChange={(e) => setCategory(e.target.value)}></input><br/>
+            <label>Product description:</label><br/>
+            <textarea name="description" id="description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea><br/>
+            <label>Number of product in stock:</label><br/>
+            <input type="number" name="countAvailable" id="countAvailable" value={countAvailable} onChange={(e) => setCountAvailable(e.target.value)}></input><br/>
+          <button className="primaryBtn" name="save">{id ?"Update": "Add"}</button><br/>
           <button className="secondaryBtn" name="back" onClick={() => setFormVisible(false)}>Back</button>
         </form>
       }
